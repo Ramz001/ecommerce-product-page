a6f4fd97b1fb5ea60793dc294f90627c41e833a3
Ramz001 <rkenjaev1@gmail.com>
2022-04-25 21:28:28 +0500

    added image zoom menu

--- src/components/productImageBox/ProductImageBox.js
+++ src/components/productImageBox/ProductImageBox.js
@@ -1,60 +1,60 @@
 import { useState } from 'react';
+import ProductImageBoxZoom from '../productImageBoxZoom/ProductImageBoxZoom'
+import ProductImageGrid from '../productImageGrid/ProductImageGrid';
 
 function ProductImageBox(){
     const [currentProductImage,setCurrentProductImage] = useState(1)
+    const [zoomPicture,setZoomPicture] = useState(false)
+
+    function handleZoomPicture(){
+        setZoomPicture(!zoomPicture)
+    }
 
     function handleClick(e){
         setCurrentProductImage(e)
     }
 
-    const smallImgStyle = (n) => `w-full rounded-md hover:opacity-50 transition duration-500 ease-out 
-        cursor-pointer ${currentProductImage === n ? ' opacity-25' : ''}`
-    const smallImgContainerStyle = (n) => `${currentProductImage === n ? 'border-2 border-orange' : ''} rounded-lg `
+   function imgCarouselNext(){
+       if(currentProductImage < 4){
+            setCurrentProductImage(currentProductImage + 1)
+         }
+        else{
+            setCurrentProductImage(1)
+        }
+   }
 
+   function imgCarouselPrevious(){
+        if(currentProductImage > 1){
+            setCurrentProductImage(currentProductImage - 1)
+        }
+        else{
+            setCurrentProductImage(4)
+        }
+   }
+
+
+    console.log(zoomPicture) 
     return(
-        <div className="flex flex-col ">
+        <div className="flex flex-col">
             <img 
             src={require(`../../images/image-product-${currentProductImage}.jpg`)} 
             alt='product chosen'
-            className=' object-cover w-[22rem] h-[22rem] max-h-100 max-w-100 rounded-2xl'
-            >
-
-            </img>
-            <div className="grid grid-cols-4 gap-4 mt-4 w-[22rem] h-16">
-                <div className={smallImgContainerStyle(1)}>
-                    <img 
-                        src={require('../../images/image-product-1-thumbnail.jpg')} 
-                        alt='product 1' 
-                        onClick={() => handleClick(1)}
-                        className={smallImgStyle(1)}
-                    />
-                </div>
-                <div className={smallImgContainerStyle(2)}>
-                    <img 
-                        src={require('../../images/image-product-2-thumbnail.jpg')} 
-                        alt='product 2'
-                        onClick={() => handleClick(2)}
-                        className={smallImgStyle(2)}
-                    />
-                </div>
-                <div className={smallImgContainerStyle(3)}>
-                <img 
-                    src={require('../../images/image-product-3-thumbnail.jpg')} 
-                    alt='product 3'
-                    onClick={() => handleClick(3)}
-                    className={smallImgStyle(3)}
+            className='object-cover w-[22rem] h-[22rem] rounded-2xl'
+            onClick={handleZoomPicture}
+            />
+            <ProductImageGrid 
+                handleClick={handleClick} 
+                currentProductImage={currentProductImage}
+                gridWidth={'w-[22rem]'}
                 />
-                </div>
-                <div className={smallImgContainerStyle(4)}>
-                    <img 
-                        src={require('../../images/image-product-4-thumbnail.jpg')} 
-                        alt='product 4'
-                        onClick={() => handleClick(4)}
-                        className={smallImgStyle(4)}
-
-                    />
-                </div>
-            </div>      
+            
+            {zoomPicture && <ProductImageBoxZoom 
+                handleClick={handleClick} 
+                currentProductImage={currentProductImage} 
+                handleZoomPicture={handleZoomPicture}
+                imgCarouselNext={imgCarouselNext}
+                imgCarouselPrevious={imgCarouselPrevious}
+                />}   
         </div>
     )
 }
---
+++ src/components/productImageBoxZoom/ProductImageBoxZoom.js
@@ -0,0 +1,58 @@
+import closeLogo from '../../images/icon-close.svg'
+import nextLogo from '../../images/icon-next.svg'
+import previousLogo from '../../images/icon-previous.svg'
+import ProductImageGrid from '../productImageGrid/ProductImageGrid';
+
+
+function ProductImageBoxZoom(props){
+    const { handleZoomPicture, handleClick, currentProductImage, imgCarouselNext, imgCarouselPrevious } = props
+    
+    return(
+        <article>
+            <div className="absolute w-screen h-screen left-[0] right-0 flex items-center justify-center 
+            top-0 bg-black-low-opacity">
+                <div className="flex flex-col relative">
+                    <img 
+                        src={closeLogo} 
+                        alt="close logo"
+                        className='absolute -top-8 right-0 cursor-pointer hover:opacity-60'
+                        onClick={handleZoomPicture}
+                    />
+                    <div className='bg-white px-5 p-4 rounded-full absolute -left-6 bottom-[54%]
+                    hover:opacity-75 transition duration-300 ease-out cursor-pointer'
+                    onClick={imgCarouselPrevious}>
+                        <img
+                            src={previousLogo}
+                            alt="previous logo"
+                            className=''
+                        />
+                    </div>
+                    <div className='bg-white px-5 p-4 rounded-full absolute -right-6 bottom-[54%]
+                    hover:opacity-75 transition duration-300 ease-out cursor-pointer'
+                    onClick={imgCarouselNext}
+                    >
+                        <img
+                            src={nextLogo}
+                            alt="next logo"
+                            className=''
+                        />
+                    </div>
+                    <img>
+                    </img>
+                    <img 
+                        src={require(`../../images/image-product-${currentProductImage}.jpg`)} 
+                        alt='product chosen'
+                        className='object-cover w-[30rem] h-[30rem] rounded-2xl'
+                    />
+                    <ProductImageGrid 
+                        handleClick={handleClick} 
+                        currentProductImage={currentProductImage}
+                        gridWidth={'w-[24rem]'}
+                        />
+                </div>
+            </div>
+        </article>
+    )
+}
+
+export default ProductImageBoxZoom;
